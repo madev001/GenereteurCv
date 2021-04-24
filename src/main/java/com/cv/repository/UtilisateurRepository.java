5330617df4170014319ea571f6aa7895e4435b65
ALOUAN Mouad <marocoujdamouad@gmail.com>
2021-04-24 01:17:59 +0000

    add formation form Jquery

--- src/main/java/com/cv/repository/UtilisateurRepository.java
+++ src/main/java/com/cv/repository/UtilisateurRepository.java
@@ -2,8 +2,7 @@ package com.cv.repository;
 
 import com.cv.entities.*;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
