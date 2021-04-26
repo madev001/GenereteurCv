3bf9bc972a20c9ca1598becee134483f56ef7bbc
ALOUAN Mouad <marocoujdamouad@gmail.com>
2021-04-26 00:28:56 +0000

    experience/formation affichage stockage complete

--- src/main/java/com/cv/controller/ExperienceController.java
+++ src/main/java/com/cv/controller/ExperienceController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cv.entities.Experience;
+import com.cv.entities.Formation;
 import com.cv.entities.Utilisateur;
 import com.cv.repository.UtilisateurRepository;
 import com.cv.service.ExperienceService;
@@ -36,7 +37,7 @@ public class ExperienceController {
 	}
 	
 	@RequestMapping(value = "/saveExperience", method = { RequestMethod.GET, RequestMethod.POST })
-    public ModelAndView addExperience( @ModelAttribute Experience experience,@RequestParam("id") Long id/*, BindingResult bindingResult*/) {
+    public ModelAndView addExperience(@ModelAttribute Experience experience,@RequestParam("id") Long id/*, BindingResult bindingResult*/) {
 		/*if(bindingResult.hasErrors())
 		{
 	        ModelAndView mav = new ModelAndView("Carriere");
@@ -53,4 +54,17 @@ public class ExperienceController {
         mav.addObject("utilisateur",u);
         return mav;
     }
+	
+	@RequestMapping(value = "/deleteExperience",method = RequestMethod.GET)
+	public ModelAndView suppFormation(@RequestParam("Experience_id") Long Experience_id,@RequestParam("Utilisateur_id") Long Utilisateur_id)
+	{
+		ModelAndView mav = new ModelAndView("MesExperiences");
+		es.deleteExperience(Experience_id);
+		Utilisateur u = ur.getUtilisateur(Utilisateur_id);
+		List<Experience> l = es.getAllExperience(u);
+        mav.addObject("experiences",l);
+        mav.addObject("experience",new Experience());
+        mav.addObject("utilisateur",u);
+	        return mav;
+	}
 }
--- src/main/java/com/cv/entities/Experience.java
+++ src/main/java/com/cv/entities/Experience.java
@@ -14,9 +14,7 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name = "Experiences")
 public class Experience implements Serializable{
@@ -28,7 +26,87 @@ public class Experience implements Serializable{
     private Long id;
     
     private String Fonction;
-    private String Localite;
+    public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getFonction() {
+		return Fonction;
+	}
+
+
+	public void setFonction(String fonction) {
+		Fonction = fonction;
+	}
+
+
+	public String getLocalite() {
+		return Localite;
+	}
+
+
+	public void setLocalite(String localite) {
+		Localite = localite;
+	}
+
+
+	public String getEtablissement() {
+		return Etablissement;
+	}
+
+
+	public void setEtablissement(String etablissement) {
+		Etablissement = etablissement;
+	}
+
+
+	public String getDepuis() {
+		return Depuis;
+	}
+
+
+	public void setDepuis(String depuis) {
+		Depuis = depuis;
+	}
+
+
+	public String getJusqua() {
+		return Jusqua;
+	}
+
+
+	public void setJusqua(String jusqua) {
+		Jusqua = jusqua;
+	}
+
+
+	public String getDescription() {
+		return Description;
+	}
+
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+
+	private String Localite;
     private String Etablissement;
     private String Depuis;
     private String Jusqua;
@@ -36,6 +114,6 @@ public class Experience implements Serializable{
 
     
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_utilisateur",referencedColumnName ="id", insertable = false, updatable = false)
+    @JoinColumn(name = "id_utilisateur",referencedColumnName ="id", updatable = false)
     private Utilisateur utilisateur;
 }
