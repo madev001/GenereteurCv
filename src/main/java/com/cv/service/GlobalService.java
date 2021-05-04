23770f48c60b2d194e46ffad42a85918e71819d6
ALOUAN Mouad <marocoujdamouad@gmail.com>
2021-05-04 00:17:16 +0000

    add skills to model1

--- src/main/java/com/cv/service/GlobalService.java
+++ src/main/java/com/cv/service/GlobalService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cv.entities.Competance;
 import com.cv.entities.Experience;
 import com.cv.entities.Formation;
 import com.cv.entities.Langue;
 import com.cv.entities.Utilisateur;
+import com.cv.repository.CompetanceRepository;
 import com.cv.repository.UtilisateurRepository;
 
 @Service
@@ -22,6 +24,8 @@ public class GlobalService {
 	@Autowired
 	LangueService ls;
 	@Autowired
+	CompetanceRepository cr;
+	@Autowired
 	UtilisateurRepository ur;
 	
 	public  ModelAndView getAllinfo(Long id)
@@ -31,10 +35,12 @@ public class GlobalService {
 		List<Formation> formations = fs.listeFormation(u);
 		List<Experience> experiences = es.getAllExperience(u);
 		List<Langue> langues = ls.getAllLangue(u);
+		List<Competance> competances = cr.getAllCompetance(u);
 		mav.addObject("utilisateur",u);
 		mav.addObject("formations",formations);
 		mav.addObject("langues",langues);
 		mav.addObject("experiences",experiences);
+		mav.addObject("competances",competances);
         return mav;
 		
 	}
